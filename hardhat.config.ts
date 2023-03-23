cb1cd3972c911f179e96eb282ea49915e04b68df
Archisman Das <72722967+Arch0125@users.noreply.github.com>
2023-03-23 17:31:10 +0530

    add hardhat deployment config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY || ""]
+    }
+  },
 };
 
 export default config;
